46a69f9713e29b9807426786ecafb7f58bfa7c5d
Luis Martinez <luis_mmartinez@live.com>
2022-03-26 19:03:18 -0400

    ad request headers parsing

--- Starting a Server/app/index.js
+++ Starting a Server/app/index.js
@@ -6,15 +6,21 @@ let server = http.createServer(function(req, res){
     const parsedUrl = url.parse(req.url, true);
 
     const path = parsedUrl.pathname;
+    
     const trimmedPath = path.replace(/^\/+|\/+$/g,'');
-
-    const method = req.method.toLowerCase();
 
     const queryStringObject = parsedUrl.query;
 
+    const method = req.method.toLowerCase();
+
+    const headers = req.headers;    
+
     res.end('Hello World\n'); 
 
-    console.log('Request received on path: '+trimmedPath+ ' with method: '+method+' and with this query string parameters', queryStringObject);
+    // console.log('Request received on path: '+trimmedPath+ ' with method: '+method+' and with this query string parameters', queryStringObject);
+    console.log('Reqeust received with these headers: ', headers);
+
+
 })
 
 server.listen(3000, function(){
