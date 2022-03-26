7abfb09d8a00c4e8d83d6f28446301c5d9e7bc38
Luis Martinez <luis_mmartinez@live.com>
2022-03-26 18:35:07 -0400

    add request method

--- Starting a Server/app/index.js
+++ Starting a Server/app/index.js
@@ -5,19 +5,14 @@ let server = http.createServer(function(req, res){
 
     const parsedUrl = url.parse(req.url, true);
 
-
     const path = parsedUrl.pathname;
-
-
     const trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
+    const method = req.method.toLowerCase();
 
     res.end('Hello World\n'); 
 
-    console.log('Request received on path: '+trimmedPath)
-
-    
-
+    console.log('Request received on path: '+trimmedPath+ 'with method: '+method);
 })
 
 server.listen(3000, function(){
