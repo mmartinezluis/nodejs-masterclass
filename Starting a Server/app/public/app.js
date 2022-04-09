ff188aaf64fb9fb2847a572c659bc04ce0725e8b
Luis Martinez <luis_mmartinez@live.com>
2022-04-09 16:29:50 -0400

    add javascript to process http requests client side

--- Starting a Server/app/public/app.js
+++ Starting a Server/app/public/app.js
@@ -1,7 +1,85 @@
 /*
 * Frontend logic for the Application
 */
-
+// Container for the frontend application
 let app = {};
+// console.log('Hello console world');
 
-console.log('Hello console world');
+// Config
+app.config = {
+    'sessionToken' : false
+};
+
+// AJAX Client for the restful API
+app.client = {};
+
+// Interface for making API calls
+app.client.request = function(headers,path,method,queryStringObject,payload,callback){
+
+    // Set defaults
+    headers = typeof(headers) == 'object' && headers !== null ? headers : {};
+    path = typeof(path) == 'string' ? path : "/";
+    method = typeof(method) == 'string' && ['POST','GET','PUT','DELETE'].indexOf(method) > -1 ? method.toUpperCase() : 'GET';
+    queryStringObject = typeof(queryStringObject) == 'object' && queryStringObject !== null ? queryStringObject : {};
+    payload = typeof(payload) == 'object' && payload !== null ? payload : {};
+    callback = typeof(callback) == 'function' ? callback : false;
+
+
+    // For each query string parameter sent, add it to the path
+    let requestUrl = path+'?';
+    let counter = 0;
+    for(let queryKey in queryStringObject){
+        if(queryStringObject.hasOwnProperty(queryKey)){
+            counter++;
+            // If at least one query string parameter has already been added, prepend new ones qith an anpersand
+            if(counter > 1){
+                requestUrl+='&';
+            }
+            // Add the key value
+            requestUrl+=queryKey+'='+queryStringObject[queryKey]
+        }
+    }
+
+    // Form the http equest as a JSON type
+    let xhr = new XMLHttpRequest();
+    xhr.open(method,requestUrl,true);
+    xhr.setRequestHeader('Content-Type','applciation/json');
+
+    // For each header sent, add it to the request
+    for(let headerKey in headers){
+        if(headers.hasOwnProperty(headerKey)){
+            xhr.setRequestHeader(headerKey,headers[headerKey]);
+        }
+    }
+
+    // If there is a current session token, add that as a header
+    if(app.config.sessionToken){
+        xhr.setRequestHeader('token',app.config.sessionToken.id);
+    }
+
+    // When the request comes back, handle the response
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == XMLHttpRequest.DONE){
+            let statusCode = xhr.status;
+            let responseReturend = xhr.responseText;
+
+            //  Callback if requested
+            if(callback){
+                try{
+                    let parsedResponse = JSON.parse(responseReturend);
+                    callback(statusCode,parsedResponse);
+                } catch(e){
+                    callback(statusCode,false);
+                }
+            }
+        }
+    };
+
+    // Send the payload as JSON
+    let payloadString = JSON.stringify(payload);
+    xhr.send(payloadString);
+ 
+};
+
+
+
